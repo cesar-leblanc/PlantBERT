c4d1f32cc987f01f5c5e84750358410db24e152f
cesar-leblanc <cesar.leblanc@inria.fr>
2025-09-12 09:52:59 +0200

    Correct typo in the utility functions

--- Scripts/utils.py
+++ Scripts/utils.py
@@ -15,7 +15,7 @@ import subprocess
 
 def bool_type(v):
     if isinstance(v, bool):
-       return v
+        return v
     if v.lower() in ('yes', 'true', 't', 'y', '1'):
         return True
     elif v.lower() in ('no', 'false', 'f', 'n', '0'):
